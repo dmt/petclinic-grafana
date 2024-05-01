0fa6c6f9e51d6e28e4fb8a403033164d73d747c9
Daniel Temme <daniel.temme@projectolympus.co.za>
2024-05-01 12:26:39 +0200

    add loki config overrides
    filter heatmap uris
    
    config adjustments

--- script.js
+++ script.js
@@ -18,40 +18,16 @@ export const options = {
 
       stages: [
         // ramp up iterations per `timeUnit` for the first minute.
+        {target: 400, duration: '1m'},
+        // keep ramping up
+        {target: 800, duration: '5m'},
+        // hold
         {target: 800, duration: '1m'},
-
-        // keep going
-        {target: 800, duration: '2m'},
-
-        // Linearly ramp-down
+        // ramp-down
         {target: 50, duration: '1m'},
       ],
     },
   },
-  // Uncomment this section to enable the use of Browser API in your tests.
-  //
-  // See https://grafana.com/docs/k6/latest/using-k6-browser/running-browser-tests/ to learn more
-  // about using Browser API in your test scripts.
-  //
-  // scenarios: {
-  //   // The scenario name appears in the result summary, tags, and so on.
-  //   // You can give the scenario any name, as long as each name in the script is unique.
-  //   ui: {
-  //     // Executor is a mandatory parameter for browser-based tests.
-  //     // Shared iterations in this case tells k6 to reuse VUs to execute iterations.
-  //     //
-  //     // See https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ for other executor types.
-  //     executor: 'shared-iterations',
-  //     options: {
-  //       browser: {
-  //         // This is a mandatory parameter that instructs k6 to launch and
-  //         // connect to a chromium-based browser, and use it to run UI-based
-  //         // tests.
-  //         type: 'chromium',
-  //       },
-  //     },
-  //   },
-  // }
 };
 
 // The function that defines VU logic.
